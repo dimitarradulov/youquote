f767cc8f7b78f0e43a2d6fd3cc91a99d1ed15287
Dimitar Radulov <76252856+dimitarradulov@users.noreply.github.com>
2022-06-26 15:31:31 +0300

    Add auth subscribtion to track if user is authenticated

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
   isNavbarCollapsed = false;
   isAuth = false;
+  authSubscribtion: Subscription;
 
-  constructor() {}
+  constructor(private authService: AuthService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.authSubscribtion = this.authService.user.subscribe((userData) => {
+      this.isAuth = userData ? true : false;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.authSubscribtion.unsubscribe();
+  }
 }
