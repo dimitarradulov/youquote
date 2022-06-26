a2b18f6c70f37db9cc8062b35d14f40a373ac143
Dimitar Radulov <76252856+dimitarradulov@users.noreply.github.com>
2022-06-26 15:30:20 +0300

    Add signIn, signUp, saveUser methods + error handling

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,9 +1,84 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+import { environment } from 'src/environments/environment';
+import { User } from './user.model';
+
+export interface AuthResponseData {
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+  registered?: string;
+}
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  user = new BehaviorSubject<User | null>(null);
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  saveUser(user: User) {
+    localStorage.setItem('user', JSON.stringify(user));
+  }
+
+  signUp(email: string, password: string) {
+    return this.http
+      .post<AuthResponseData>(
+        `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${environment.apiKey}`,
+        {
+          email,
+          password,
+          returnSecureToken: true,
+        }
+      )
+      .pipe(catchError(this.handleError));
+  }
+
+  signIn(email: string, password: string) {
+    return this.http
+      .post<AuthResponseData>(
+        `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.apiKey}`,
+        {
+          email,
+          password,
+          returnSecureToken: true,
+        }
+      )
+      .pipe(catchError(this.handleError));
+  }
+
+  handleError(errResponse: HttpErrorResponse) {
+    let errorMsg = 'An unknown error occured!';
+
+    if (!errResponse.error.error) return throwError(() => new Error(errorMsg));
+
+    switch (errResponse.error.error.message) {
+      case 'EMAIL_EXISTS':
+        errorMsg = 'The email address is already in use by another account.';
+        break;
+      case 'TOO_MANY_ATTEMPTS_TRY_LATER':
+        errorMsg =
+          'We have blocked all requests from this device due to unusual activity. Try again later.';
+        break;
+      case 'EMAIL_NOT_FOUND':
+        errorMsg =
+          'There is no user record corresponding to this identifier. The user may have been deleted.';
+        break;
+      case 'INVALID_PASSWORD':
+        errorMsg =
+          'The password is invalid or the user does not have a password.';
+        break;
+      case 'USER_DISABLED':
+        errorMsg = 'The user account has been disabled by an administrator.';
+        break;
+    }
+
+    return throwError(() => new Error(errorMsg));
+  }
 }
