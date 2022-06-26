8fc01ef516d648b42b88e2e66e7ef2696cd00e9b
Dimitar Radulov <76252856+dimitarradulov@users.noreply.github.com>
2022-06-26 12:54:58 +0300

    Add web api key prop

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiKey: 'AIzaSyBgHTItG9M33VSXuBJ-TTz0nP4d6lVlCdU',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: 'AIzaSyBgHTItG9M33VSXuBJ-TTz0nP4d6lVlCdU',
 };
 
 /*
