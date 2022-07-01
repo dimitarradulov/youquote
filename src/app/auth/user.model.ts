38119f266c64487e9a9e3da838b7617244183efd
Dimitar Radulov <76252856+dimitarradulov@users.noreply.github.com>
2022-07-01 18:25:33 +0300

    Add get token

--- src/app/auth/user.model.ts
+++ src/app/auth/user.model.ts
@@ -5,4 +5,10 @@ export class User {
     private _token: string,
     private _tokenExpiration: Date
   ) {}
+
+  get token() {
+    if (new Date() > this._tokenExpiration) return null;
+
+    return this._token;
+  }
 }
