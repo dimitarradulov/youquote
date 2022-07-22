0290ce46898ab0d670d7e06505803cb49a84cbd4
Dimitar Radulov <76252856+dimitarradulov@users.noreply.github.com>
2022-07-22 15:04:38 +0300

    Update quotes to be stored in a property

--- src/app/my-quotes/my-quotes.component.ts
+++ src/app/my-quotes/my-quotes.component.ts
@@ -1,16 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 import { User } from '../auth/user.model';
 import { AuthService } from '../auth/auth.service';
 import { QuotesService } from '../quotes/quotes.service';
 import { LoadingService } from '../shared/loading-spinner/loading.service';
+import { Quote } from '../quotes/quote.model';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-my-quotes',
   templateUrl: './my-quotes.component.html',
   styleUrls: ['./my-quotes.component.scss'],
 })
-export class MyQuotesComponent implements OnInit {
+export class MyQuotesComponent implements OnInit, OnDestroy {
+  userQuotes: Quote[] = [];
+  showConfirmModal = false;
+  confirmQuoteDelete = false;
+  subsciption: Subscription;
+
   constructor(
     private authService: AuthService,
     public quotesService: QuotesService,
@@ -20,5 +27,17 @@ export class MyQuotesComponent implements OnInit {
   ngOnInit(): void {
     const user = <User>this.authService.user.getValue();
     this.quotesService.getUserQuotes(user.id);
+
+    this.subsciption = this.quotesService.userQuotesChanges.subscribe(
+      (quotes) => (this.userQuotes = quotes)
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subsciption.unsubscribe();
+  }
+
+  hideConfirmModal() {
+    this.showConfirmModal = false;
   }
 }
