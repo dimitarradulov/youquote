e06119a532a901ec228dbc616ff5be9d631b5ee0
Dimitar Radulov <76252856+dimitarradulov@users.noreply.github.com>
2022-07-26 18:12:08 +0300

    Add like quote logic

--- src/app/quotes/quote-details/quote-details.component.ts
+++ src/app/quotes/quote-details/quote-details.component.ts
@@ -2,9 +2,12 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs';
 
+import { User } from 'src/app/auth/user.model';
+import { Quote } from '../../shared/models/quote.model';
+import { AuthService } from 'src/app/auth/auth.service';
 import { LoadingService } from 'src/app/shared/loading-spinner/loading.service';
-import { Quote } from '../quote.model';
 import { QuotesService } from '../quotes.service';
+import { LikeService } from './like/like.service';
 
 @Component({
   selector: 'app-quote-details',
@@ -13,21 +16,31 @@ import { QuotesService } from '../quotes.service';
 })
 export class QuoteDetailsComponent implements OnInit, OnDestroy {
   quote: Quote;
+  quoteId: string;
+  liked: boolean;
+  likes = 0;
+  user: User | null;
   error = null;
   subsciption: Subscription;
 
   constructor(
     private quotesService: QuotesService,
     private route: ActivatedRoute,
+    private likeService: LikeService,
+    public authService: AuthService,
     public loadingService: LoadingService
   ) {}
 
   ngOnInit(): void {
-    const { quoteId } = this.route.snapshot.params;
+    this.quoteId = this.route.snapshot.params['quoteId'];
+
+    this.user = this.authService.user.getValue();
 
     this.error = null;
-    this.subsciption = this.quotesService.getOne(quoteId).subscribe({
-      next: (quoteData) => (this.quote = quoteData),
+    this.subsciption = this.quotesService.getOne(this.quoteId).subscribe({
+      next: (quoteData) => {
+        this.quote = quoteData;
+      },
       error: (err) => {
         console.error(err);
         this.error = err.error?.error
@@ -35,9 +48,22 @@ export class QuoteDetailsComponent implements OnInit, OnDestroy {
           : 'Error fetching the quote details. Please try again!';
       },
     });
+
+    this.likeService.getLikesForSingleQuote(this.quoteId).subscribe((likes) => {
+      this.likes = likes.length;
+      this.liked = likes.some((uid) => uid === this.user?.id);
+    });
   }
 
   ngOnDestroy(): void {
     this.subsciption.unsubscribe();
+  }
+
+  onLikeHandler(num: number) {
+    this.likes += num;
+    let quoteLikes = this.likeService.quoteLikes.getValue();
+    if (this.liked) quoteLikes.push(<string>this.user?.id);
+    else quoteLikes = quoteLikes.filter((uid) => uid !== <string>this.user?.id);
+    this.likeService.modifyLikes(this.quoteId, quoteLikes);
   }
 }
